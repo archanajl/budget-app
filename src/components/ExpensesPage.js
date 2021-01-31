c574f745794614a23295e0e294d807c40a866706
archanajl <65244537+archanajl@users.noreply.github.com>
2021-01-31 13:12:03 +0000

    Added Expenses Summary - Count & Total

--- src/components/ExpensesPage.js
+++ src/components/ExpensesPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ExpensesList from './ExpensesList';
 import ExpenseListFilters from './ExpenseListFilters';
+import ExpensesSummary from './ExpensesSummary';
 
 
 const ExpensesPage = () => (
     <div>
         <ExpenseListFilters/> 
+        <ExpensesSummary />
         <ExpensesList/>
     </div>
 )
